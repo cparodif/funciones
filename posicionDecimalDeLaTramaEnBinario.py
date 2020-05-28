51ef58dd331554e12a7172638332854a2ba1bbb0
SolRac <36419016+cparodif@users.noreply.github.com>
2020-05-28 20:06:13 +0200

    posicionDecimalDeLaTramaEnBinario.py

---
+++ posicionDecimalDeLaTramaEnBinario.py
@@ -0,0 +1,51 @@
+def posicionDecimalDeLaTramaEnBinario(pTrama, pPosicion, pLongitud):
+    numCaracteres = len(pPosicion)
+    if (numCaracteres == 1):
+        x = pPosicion
+    if (numCaracteres == 2):
+        x = pPosicion
+    if (numCaracteres == 3):
+        if (pPosicion[1] == "."):
+            x = pPosicion
+            
+        if (pPosicion[1] == "-"):
+            x = pPosicion
+
+    if (pLongitud == "1 bit")
+         y = 1
+    if (pLongitud == "2 bits")
+         y = 2
+    if (pLongitud == "3 bits")
+         y = 3
+    if (pLongitud == "4 bits")
+         y = 4
+    if (pLongitud == "5 bits")
+        y = 5
+    if (pLongitud == "6 bits")
+        y = 6
+    if (pLongitud == "7 bits")
+        y = 7
+    if (pLongitud == "8 bits")
+        y = 8
+    if (pLongitud == "9 bits")
+        y = 9
+    if (pLongitud == "10 bits")
+        y = 10
+    if (pLongitud == "1 byte")
+        y = 8
+    if (pLongitud == "2 bytes")
+        y = 16
+    if (pLongitud == "3 bytes")
+        y = 24
+    if (pLongitud == "4 bytes")
+        y = 32
+
+
+
+
+def datosDeLaTrama(pTrama, pPosicion, pLongitud):
+    posicionInical= ((int(pPsicion[0])-1)*8)+int(pPsicion[1])-1
+    longitud = plongitud
+    xTrama = pTrama[]
+
+def datosEstanDisponibles():
