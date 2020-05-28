1d95b724c990a006990677478e1358eadba78eff
SolRac <36419016+cparodif@users.noreply.github.com>
2020-05-28 20:00:28 +0200

    Update hexadecimalEnBinario.py

--- hexadecimalEnBinario.py
+++ hexadecimalEnBinario.py
@@ -3,6 +3,14 @@ def calcularCanId(p):
     x = x[-8:]
     return x
 
+def calcularTrama(p):
+    trama = p[2:25]
+    return trama
+
+def calcularTramab(p):
+    x=hexadecimalEnBinario(calcularTrama(p))
+    return x
+
 def hexadecimalEnBinario(p):
     valoresHexadecimalBinario={'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101',
         '6':'0110','7':'0111','8':'1000','9':'1001','a':'1010','b':'1011',
@@ -13,3 +21,4 @@ def hexadecimalEnBinario(p):
         if i in valoresHexadecimalBinario:
             x.append(valoresHexadecimalBinario[i])
     return ("").join(x)
+
