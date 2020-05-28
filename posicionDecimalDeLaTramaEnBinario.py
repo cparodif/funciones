48f5db01418daf3ae8db3b11a22ba3faf6b78a63
SolRac <36419016+cparodif@users.noreply.github.com>
2020-05-28 21:02:42 +0200

    Update posicionDecimalDeLaTramaEnBinario.py

--- posicionDecimalDeLaTramaEnBinario.py
+++ posicionDecimalDeLaTramaEnBinario.py
@@ -1,33 +1,5 @@
-def posicionDecimalDeLaTramaEnBinario(pTramah, pPosicion, pLongitud):
-# posicionDecimalDeLaTramaEnBinario("00 00 00 0c 06 54 23 41", "1.3", "2 bits"):    
-    # posicion "1.3" o "2-3"
-    pTrama[0] = pTramah[0:2]
-    pTrama[1] = pTramah[3:5]
-    pTrama[2] = pTramah[6:8]
-    pTrama[3] = pTramah[9:11]
-    pTrama[4] = pTramah[12:14]
-    pTrama[5] = pTramah[15:17]
-    pTrama[6] = pTramah[18:20]
-    pTrama[7] = pTramah[21:23]
-    
-    numCaracteres = len(pPosicion)
-    if (numCaracteres == 1):
-        x = pPosicion
-    if (numCaracteres == 2):
-        x = pPosicion
-        
-    if (numCaracteres == 3):
-        if (pPosicion[1] == "."):
-            x= ((int(pPsicion[0])-1)*8)+int(pPsicion[1])-1
-            datosParaAnalizar = hexadecimalEnBinario(pTrama[int(pPosicion[0])])
-            
-        if (pPosicion[1] == "-"):
-            #inluir los dos bytes, de la trama pero en orden inverso
-            x1 = int(pPosicion[0])-1
-            x2 = int(pPosicion[2])-1
-            datosParaAnalizar = hexadecimalEnBinario(x2)+hexadecimalEnBinario(x1)
-            
-
+def numeroDeBits():
+    y=0
     if (pLongitud == "1 bit")
          y = 1
     if (pLongitud == "2 bits")
@@ -56,12 +28,40 @@ def posicionDecimalDeLaTramaEnBinario(pTramah, pPosicion, pLongitud):
         y = 24
     if (pLongitud == "4 bytes")
         y = 32
-    
-    
-    
+    return y
+
+def posicionDecimalDeLaTramaEnBinario(pTramah, pPosicion, pLongitud):
+# posicionDecimalDeLaTramaEnBinario("00 00 00 0c 06 54 23 41", "1.3", "2 bits"):
+    pTramah.strip()
+    pTrama = pTramah.split(" ")
+    y = numeroDeBits(pLongitud)
+    # posicion "1.3" o "2-3"
+    #pTrama[0] = pTramah[0:2]
+    #pTrama[1] = pTramah[3:5]
+    #pTrama[2] = pTramah[6:8]
+    #pTrama[3] = pTramah[9:11]
+    #pTrama[4] = pTramah[12:14]
+    #pTrama[5] = pTramah[15:17]
+    #pTrama[6] = pTramah[18:20]
+    #pTrama[7] = pTramah[21:23]
+
+    numCaracteres = len(pPosicion)
+    if (numCaracteres == 1):
+        x = pPosicion[0]
+        datosParaAnalizar = hexadecimalEnBinario(pTrama[int(pPosicion[0])])
+
+    if (numCaracteres == 3):
+        if (pPosicion[1] == "."):
+            x= ((int(pPsicion[0])-1)*8)+int(pPsicion[2])-1
+            datosParaAnalizar = hexadecimalEnBinario(pTrama[int(pPosicion[0])])
+
+        if (pPosicion[1] == "-"):
+            #inluir los dos bytes, de la trama pero en orden inverso
+            x1 = int(pPosicion[0])-1
+            x2 = int(pPosicion[2])-1
+            datosParaAnalizar = hexadecimalEnBinario(x2)+hexadecimalEnBinario(x1)
+
     return datosParaAnalizar
 
-
-    
 
 def datosEstanDisponibles():
